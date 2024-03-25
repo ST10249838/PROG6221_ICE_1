13f5c82825b45a28e3f0684c01b34692f4f3d274
Blaise Mikka de Gier <ST10249838@vcconnect.edu.za>
2024-03-25 21:59:49 +0200

    Program

--- ShoppingCart/Program/Program.cs
+++ ShoppingCart/Program/Program.cs
@@ -8,17 +8,13 @@ namespace Program
         {
             // Create Shopping Cart
             ShoppingCart shoppingCart = new ShoppingCart(5);
-            // Create Products
-            ClothingProduct shirt = new ClothingProduct("Shirt", 10, ProductCategory.Clothing, "L", "White");
-            ElectronicsProduct phone = new ElectronicsProduct("Phone", 100, ProductCategory.Electronics, "Apple", "15");
-            // Add Products
-            shoppingCart.AddProduct(phone);
-            shoppingCart.AddProduct(shirt);
-            phone.GetInfo();
-            Console.WriteLine("\n");
-            shirt.GetInfo();
-            // Remove Product
-            shoppingCart.RemoveProduct(phone);
+            
+            // Create products to add to shopping cart
+            ClothingProduct shirt = new ClothingProduct("Shirt", 10, ProductCategory.Clothing, "M", "Blue");
+            ElectronicsProduct phone = new ElectronicsProduct("Phone", 1000, ProductCategory.Electronics, "Samsung", "Galaxy S21");
+            HomeProduct chair = new HomeProduct("Chair", 50, ProductCategory.Home, "21m", "Brown");
+            BeautyProduct lipstick = new BeautyProduct("Lipstick", 5, ProductCategory.Beauty, "Matte", "Red");
+            GroceriesProduct milk = new GroceriesProduct("Milk", 2, ProductCategory.Groceries, 24);
         }
     }
 }
