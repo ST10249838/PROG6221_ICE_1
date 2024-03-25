4ea7721e0c245af6742ffea68f452750e339cf69
Blaise Mikka de Gier <ST10249838@vcconnect.edu.za>
2024-03-25 21:55:41 +0200

    HomeProduct

---
+++ ShoppingCart/ShoppingCartApp/HomeProduct.cs
@@ -0,0 +1,36 @@
+﻿namespace ShoppingCartApp
+{
+    // A derived class of Product for home products.
+    public class HomeProduct : Product
+    {
+        // Fields to include:
+        // The size of the home product.
+        private string size;
+        // The color of the home product.
+        private string color;
+
+        // Properties to include:
+        // Gets the size of the home product.
+        public string Size { get { return size; } }
+        // Gets the color of the home product.
+        public string Color { get { return color; } }
+
+        // Creates a new instance of the HomeProduct class with the specified name, price, category, size, and color.
+        public HomeProduct(string name, double price, ProductCategory category, string size, string color) : base(name, price, category)
+        {
+            this.size = size;
+            this.color = color;
+        }
+
+        // Methods
+        // Gets the information about the home product.
+        public override void GetInfo()
+        {
+            Console.WriteLine("Name: " + base.Name);
+            Console.WriteLine("Price: " + base.Price);
+            Console.WriteLine("Category: " + base.Category);
+            Console.WriteLine("Size: " + this.size);
+            Console.WriteLine("Color: " + this.color);
+        }
+    }
+}
