fda8bba401a2b2f51086df1028b05d4df20d6db9
Blaise Mikka de Gier <ST10249838@vcconnect.edu.za>
2024-03-25 22:39:14 +0200

    ShoppingCart2

--- ShoppingCart/ShoppingCartApp/obj/Debug/net8.0/ShoppingCartApp.AssemblyInfo.cs
+++ ShoppingCart/ShoppingCartApp/obj/Debug/net8.0/ShoppingCartApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShoppingCartApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d2bd01c74791c5ed46e953144f4b48987b7babd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d60bbc3503cbf2381b5fd3dac8a5cf0f050cf1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShoppingCartApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShoppingCartApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
