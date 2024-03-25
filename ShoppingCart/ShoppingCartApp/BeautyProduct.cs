3fa0664f2ea4de31b8abf82a8f2683823888d6b7
Blaise Mikka de Gier <ST10249838@vcconnect.edu.za>
2024-03-25 21:55:13 +0200

    BeautyProduct

---
+++ ShoppingCart/ShoppingCartApp/BeautyProduct.cs
@@ -0,0 +1,36 @@
+﻿namespace ShoppingCartApp
+{
+    // A derived class of Product for beauty products.
+    public class BeautyProduct : Product
+    {
+        // Fields to include:
+        // The brand of the beauty product.
+        private string brand;
+        // The color of the beaity product.
+        private string color;
+
+        // Properties to include:
+        // Gets the brand of the beauty product.
+        public string Brand { get { return brand; } }
+        // Gets the color of the beauty product.
+        public string Color { get { return color; } }
+
+        // Creates a new instance of the BeautyProduct class with the specified name, price, category, brand, and color.
+        public BeautyProduct(string name, double price, ProductCategory category, string brand, string color) : base(name, price, category)
+        {
+            this.brand = brand;
+            this.color = color;
+        }
+
+        // Methods
+        // Gets the information about the beauty product.
+        public override void GetInfo()
+        {
+            Console.WriteLine("Name: " + base.Name);
+            Console.WriteLine("Price: " + base.Price);
+            Console.WriteLine("Category: " + base.Category);
+            Console.WriteLine("Brand: " + this.brand);
+            Console.WriteLine("Color: " + this.color);
+        }
+    }
+}
